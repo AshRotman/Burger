613834900416dae0fe060bd8539bb882801aac71
Ash Rotman <quigibow@gmail.com>
2017-10-25 20:53:26 -0600

    added connection

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic bASH Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double bASH Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon bASH BUrger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Colorado bASH Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Milenial Avacado bASH Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bASH Sushi Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken bASH Burger', true);
